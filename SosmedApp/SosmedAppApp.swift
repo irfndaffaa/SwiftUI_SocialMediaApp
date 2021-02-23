a4c630a5988ab6952a297ba967ceedb505dc8f37
Irfandio Daffa <irfandio.daff@gmail.com>
2021-02-23 20:44:19 +0700

    Initial Commit

---
+++ SosmedApp/SosmedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SosmedAppApp.swift
+//  SosmedApp
+//
+//  Created by Irfandio Daffa on 23/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SosmedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
